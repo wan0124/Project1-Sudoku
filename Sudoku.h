e217ff3dda8863f08e046bfd854cfbc771953a20
wan0124 <jcps3503@gmail.com>
2016-04-04 22:54:10 +0800

    Update Sudoku.h

--- Sudoku.h
+++ Sudoku.h
@@ -1,21 +1,34 @@
-#include <iostream>
-class Sudoku{
-public:
-	giveQuestion();
-	readIn();
-	solve();
-	changeNum(int a,int b);
-	changeRow(int a,int b);
-	changeCol(int a,int b);
-	rotate(int n);
-	flip(int n);
-	transform();
-	change();
-	print();
-	backtrace(int count);
-	
-private:
-	int question[9][9];
-	bool 	check(int row,int col);
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
 
+using namespace std;
+class Sudoku
+{
+	public:
+		void giveQuestion();
+		void readIn();
+		void solve();
+		bool backtrack();
+		void changeNum(int a , int b);
+		void changeCol(int a , int b);
+		void changeRow(int a , int b);
+		void rotate(int n);
+		void flip(int n);
+		void transform();
+		void change();
+		void print();
+		bool checkRow(int n);
+		bool checkCol(int n);
+		bool checkCell(int n);
+		bool checkall(int col ,int row , int num);
+	private:
+		int myQ[9][9];	
+		int question[9][9];
+		int answer[9][9];
+		int ans;
+		int i;
+		int j;
 };
+
+
